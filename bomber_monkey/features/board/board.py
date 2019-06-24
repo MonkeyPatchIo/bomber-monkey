37394ea09b5d90c8b295f98254b92df468e2c0ba
flegac <florent.legac@gmail.com>
2019-06-24 16:28:39 +0200

    fix game board creation

--- bomber_monkey/features/board/board.py
+++ bomber_monkey/features/board/board.py
@@ -19,12 +19,21 @@ class Board(Component):
         self.tile_size = tile_size
         self.grid_size = grid_size
         self.grid = [Tiles.EMPTY] * (grid_size[0] * grid_size[1])
-        for i in range(10):
-            index = random.randrange(len(self.grid) - 1)
-            self.grid[index] = Tiles.WALL
-        for i in range(10):
-            index = random.randrange(len(self.grid) - 1)
-            self.grid[index] = Tiles.BLOCK
+        for i in range(25):
+            x = random.randrange(2, self.width - 2)
+            y = random.randrange(2, self.height - 2)
+            self.set(x, y, Tiles.WALL)
+        for i in range(50):
+            x = random.randrange(2, self.width - 2)
+            y = random.randrange(2, self.height - 2)
+            self.set(x, y, Tiles.BLOCK)
+
+        for x in range(self.width):
+            self.set(x, 0, Tiles.WALL)
+            self.set(x, self.height - 1, Tiles.WALL)
+        for y in range(self.height):
+            self.set(0, y, Tiles.WALL)
+            self.set(self.width - 1, y, Tiles.WALL)
 
     @property
     def width(self):
