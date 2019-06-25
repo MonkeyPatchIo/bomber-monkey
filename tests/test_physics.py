941cfea179e20a6089f2b55d0aea700b56692305
flegac <florent.legac@gmail.com>
2019-06-25 14:21:04 +0200

    add physics TU

---
+++ tests/test_physics.py
@@ -0,0 +1,43 @@
+from bomber_monkey.features.board.board import Board
+from bomber_monkey.features.physics.collision_system import PlayerWallCollisionSystem
+from bomber_monkey.features.physics.physic_system import PhysicSystem
+from bomber_monkey.features.physics.rigid_body import RigidBody
+from bomber_monkey.features.physics.shape import Shape
+from bomber_monkey.utils.vector import Vector
+from python_ecs.ecs import sim
+
+board_size = Vector.create(5, 5)
+tile_size = Vector.create(10, 10)
+
+board = sim.create(Board(
+    board_size,
+    tile_size
+))
+
+
+def test_physic_system():
+    sys = PhysicSystem(friction_ratio=.5)
+    body = RigidBody(
+        pos=Vector.create(5, 5),
+        speed=Vector.create(2, 2),
+        accel=Vector.create(0, 0)
+    )
+
+    sys.update(body)
+
+    assert body.speed == [1, 1]
+    assert body.pos == [6, 6]
+
+
+def test_player_collision():
+    sys = PlayerWallCollisionSystem(board)
+
+    body = RigidBody(
+        pos=Vector.create(3, 2.5),
+        speed=Vector.create(1, 0),
+        accel=Vector.create(0, .5)
+    )
+    shape = Shape(tile_size)
+    sys.update(body, shape)
+
+    print(body)
