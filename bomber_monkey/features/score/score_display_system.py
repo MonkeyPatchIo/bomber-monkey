30f34f5c5a89eb9afc8d2342816f03e60289e8d5
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2019-06-26 14:21:28 +0200

    clean displaying score

--- bomber_monkey/features/score/score_display_system.py
+++ bomber_monkey/features/score/score_display_system.py
@@ -1,3 +1,5 @@
+from pygame.rect import Rect
+
 from bomber_monkey.features.score.scores import Scores
 from bomber_monkey.game_config import GameConfig
 from python_ecs.ecs import System
@@ -13,6 +15,7 @@ class ScoresDisplaySystem(System):
         self.font = pg.font.Font(pygameMenu.fonts.FONT_8BIT, conf.playground_offset.y - 10)
 
     def update(self, scores: Scores) -> None:
+        self.screen.fill((0, 0, 0), Rect((0, 0), (self.conf.pixel_size.x, self.conf.playground_offset.y)))
         scoretext = self.font.render(str(scores.scores[0]), 1, (255, 255, 255))
         self.screen.blit(scoretext, (5, 3))
         scoretext = self.font.render(str(scores.scores[1]), 1, (255, 255, 255))
