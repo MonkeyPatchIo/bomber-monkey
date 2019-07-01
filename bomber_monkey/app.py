e583611f9cc78c9cee9e392f7a384b8d3f6cb2fc
flegac <florent.legac@gmail.com>
2019-07-01 23:33:12 +0200

    refacto : put FPS display in its own system

--- bomber_monkey/app.py
+++ bomber_monkey/app.py
@@ -1,5 +1,3 @@
-import random
-
 import pygame as pg
 
 from bomber_monkey.entity_factory import GameFactory
@@ -7,6 +5,7 @@ from bomber_monkey.features.board.board_display_system import BoardDisplaySystem
 from bomber_monkey.features.bomb.bomb_explosion_system import BombExplosionSystem
 from bomber_monkey.features.bomb.bomb_sound_system import BombSoundSystem
 from bomber_monkey.features.display.display_system import DisplaySystem, SpriteDisplaySystem
+from bomber_monkey.features.display.fps_display_system import FpsDisplaySystem
 from bomber_monkey.features.keyboard.keyboard_system import KeyboardSystem
 from bomber_monkey.features.lifetime.lifetime_system import LifetimeSystem
 from bomber_monkey.features.physics.collision_system import PlayerCollisionSystem
@@ -14,7 +13,7 @@ from bomber_monkey.features.physics.physic_system import PhysicSystem
 from bomber_monkey.features.player.banana_eating_system import BananaEatingSystem
 from bomber_monkey.features.player.player_controller_system import PlayerControllerSystem
 from bomber_monkey.features.player.player_killer_system import PlayerKillerSystem
-from bomber_monkey.features.player.player_score_display_system import PlayerScoreDisplaySystem
+from bomber_monkey.features.display.score_display_system import PlayerScoreDisplaySystem
 from bomber_monkey.features.tile.tile_killer_system import TileKillerSystem
 from bomber_monkey.states.app_state import AppState
 
@@ -42,8 +41,9 @@ class App:
             BananaEatingSystem(state),
             LifetimeSystem(),
 
-            BoardDisplaySystem(state.conf, state, state.conf.image_loader, state.app.screen, state.conf.tile_size),
+            BoardDisplaySystem(state.conf, state.conf.image_loader, state.app.screen, state.conf.tile_size),
             PlayerScoreDisplaySystem(state, state.app.screen),
+            FpsDisplaySystem(state, state.app.screen),
 
             DisplaySystem(state.conf, state.conf.image_loader, state.app.screen),
             SpriteDisplaySystem(state.conf, state.conf.image_loader, state.app.screen),
---
+++ bomber_monkey/features/display/fps_display_system.py
@@ -0,0 +1,32 @@
+import time
+
+import pygame as pg
+import pygameMenu
+
+from bomber_monkey.features.physics.rigid_body import RigidBody
+from bomber_monkey.features.player.player import Player
+from bomber_monkey.states.in_game import GameState
+from python_ecs.ecs import System
+
+
+class FpsDisplaySystem(System):
+    def __init__(self, state: GameState, screen):
+        super().__init__([Player])
+        self.state = state
+        self.conf = state.conf
+        self.screen = screen
+        self.nb_frames = 0
+        self.start_time = time.time()
+
+        self.font_35 = pg.font.Font(pygameMenu.fonts.FONT_8BIT, 35)
+        self.font_20 = pg.font.Font(pygameMenu.fonts.FONT_8BIT, 20)
+
+    def update(self, player: Player) -> None:
+        self.nb_frames += 1
+        fps = self.nb_frames / (time.time() - self.start_time)
+        body1: RigidBody = self.state.players[0].get(RigidBody)
+        body2: RigidBody = self.state.players[1].get(RigidBody)
+        message = 'FPS: %.2f v1x=%.2f v1y=%.2f v2x=%.2f v2y=%.2f' % (
+            fps, body1.speed.x, body1.speed.y, body2.speed.x, body2.speed.y)
+        text = self.font_20.render(message, 1, (255, 255, 255))
+        self.screen.blit(text, (5, 50))
