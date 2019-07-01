20253de20577b078a5612c1e9e8fa8cd1979d1b7
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2019-07-01 19:58:21 +0200

    add a time delta "component"

--- python_ecs/ecs.py
+++ python_ecs/ecs.py
@@ -1,3 +1,4 @@
+import time
 from typing import List, Dict, Type
 
 ComponentId = int
@@ -90,6 +91,31 @@ class Entity(object):
         return isinstance(other, Entity) and self._eid == other.eid
 
 
+class TimeDelta:
+
+    def __init__(self):
+        self._value = 0.0
+
+    def update(self):
+        self._value += 1
+
+    @property
+    def value(self) -> float:
+        return self._value
+
+
+class RealTimeDelta(TimeDelta):
+
+    def __init__(self):
+        super().__init__()
+        self._last_update_time = None
+
+    def update(self):
+        t = time.time()
+        self._value = 0.1 if self._last_update_time is None else t - self._last_update_time
+        self._last_update_time = t
+
+
 class ECS(object):
     _id_source = 0
 
@@ -99,12 +125,7 @@ class ECS(object):
         return ECS._id_source
 
     def __init__(self):
-        self._systems = []  # type: List[System]
-        self._components = {}  # type: Dict[Component.Type, Dict[EntityId,Component]]
-        self._dead = set()  #  type: Set[int]
-        self._to_create = []
-        self.on_create = []  # type: List[Callable[[Entity],None]]
-        self.on_destroy = []  # type: List[Callable[[Entity],None]]
+        self.reset()
 
     def create(self, *components) -> Entity:
         entity = Entity(self, self._generate_id())
@@ -122,22 +143,29 @@ class ECS(object):
         return Entity(self, eid)
 
     def reset_systems(self, systems: List[System]):
-        self._systems = systems
+        for system in systems:
+            self.add_system(system)
         return self
 
     def reset(self):
-        self._components = {}
-        self._systems = []
-        self._dead.clear()
-        self.on_create = []
-        self.on_destroy = []
+        self._systems = []  # type: List[System]
+        self._components = {}  # type: Dict[Component.Type, Dict[EntityId,Component]]
+        self._dead = set()  #  type: Set[int]
+        self._to_create = []
+        self.on_create = []  # type: List[Callable[[Entity],None]]
+        self.on_destroy = []  # type: List[Callable[[Entity],None]]
+        self._time_delta = RealTimeDelta()
 
     def add_system(self, system: System):
         assert isinstance(system, System)
+        set_time_delta = getattr(system, "set_time_delta", None)
+        if callable(set_time_delta):
+            set_time_delta(self._time_delta)
         self._systems.append(system)
         return self
 
     def update(self):
+        self._time_delta.update()
 
         for k in self._to_create:
             self._create_now(*k)
