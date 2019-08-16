50f752f27798a5b6cc992f137fc23bfbeeb22f08
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2019-08-16 16:39:06 +0200

    Add an animation of the count down

--- bomber_monkey/features/display/startup_count_down_display_system.py
+++ bomber_monkey/features/display/startup_count_down_display_system.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame as pg
 import pygameMenu
 
@@ -13,14 +15,24 @@ class StartupCountDownDisplaySystem(System):
     def __init__(self, conf: GameConfig, screen):
         super().__init__([Board])
         self.screen = screen
-        self.font = pg.font.Font(pygameMenu.font.FONT_8BIT, FONT_SIZE)
-        self.text_x = conf.pixel_size.x / 2
-        self.text_y = conf.pixel_size.y / 2 - FONT_SIZE / 2
 
     def update(self, sim: Simulator, dt: float, board: Board) -> None:
         conf: GameConfig = sim.context.conf
         count_down = conf.game_startup_delay - int(sim.context.game_elasped_time)
         if count_down >= 0:
             text = str(count_down) if count_down > 0 else "Go"
-            rendered_text = self.font.render(text, 1, TEXT_COLOR)
-            self.screen.blit(rendered_text, (self.text_x - len(text) * FONT_SIZE / 2, self.text_y))
+
+            ratio = sim.context.game_elasped_time - int(sim.context.game_elasped_time)
+            ratio = min(ratio * 3, 1.0)
+            font_size = int(FONT_SIZE * math.sin(ratio * math.pi / 2))
+
+            font = pg.font.Font(pygameMenu.font.FONT_8BIT, font_size)
+            text_x = conf.pixel_size.x / 2 - len(text) * font_size / 2
+            text_y = conf.pixel_size.y / 2 - font_size / 2
+
+            if count_down == 1:
+                # special case for '1' since it is not centered in the font
+                text_x += font_size / 4
+
+            rendered_text = font.render(text, 1, TEXT_COLOR)
+            self.screen.blit(rendered_text, (text_x, text_y))
