00d62a144fef30949eae9f2849406b84979ab64e
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2019-08-18 20:14:41 +0200

    use the monkeypatch logo

--- bomber_monkey/features/display/title_bar_display_system.py
+++ bomber_monkey/features/display/title_bar_display_system.py
@@ -2,16 +2,19 @@ import pygame as pg
 import pygameMenu
 
 from bomber_monkey.features.board.board import Board
+from bomber_monkey.features.display.image import Image
 from bomber_monkey.features.physics.rigid_body import RigidBody
 from bomber_monkey.game_config import BLUE_MONKEY_COLOR, BLACK_COLOR, WHITE_COLOR, GameConfig
 from bomber_monkey.utils.vector import Vector
 from python_ecs.ecs import System, Simulator
 
 TITLE_FONT_SIZE = 35
-SUBTITLE_FONT_SIZE = 20
 TITLE = "Bomber Monkey"
-SUBTITLE = "by Monkey Patch"
 MARGIN = 3
+SUBTITLE_FONT_SIZE = 20
+SUBTITLE_PREFIX = "by "
+SUBTITLE_PICTURE_SIZE = Vector.create(230, 46)
+SUBTITLE_SIZE = Vector.create((len(SUBTITLE_PREFIX)) * SUBTITLE_FONT_SIZE, 0) + SUBTITLE_PICTURE_SIZE
 
 
 class TitleBarDisplaySystem(System):
@@ -22,10 +25,14 @@ class TitleBarDisplaySystem(System):
         font_title = pg.font.Font(pygameMenu.font.FONT_8BIT, TITLE_FONT_SIZE)
         self.font_subtitle = pg.font.Font(pygameMenu.font.FONT_8BIT, SUBTITLE_FONT_SIZE)
         self.rendered_title = font_title.render(TITLE, 1, BLUE_MONKEY_COLOR)
-        self.rendered_subtitle = self.font_subtitle.render(SUBTITLE, 1, BLUE_MONKEY_COLOR)
         self.title_pos = Vector.create(conf.pixel_size.x / 2 - (len(TITLE) * TITLE_FONT_SIZE) / 2, MARGIN)
-        self.subtitle_pos = Vector.create(conf.pixel_size.x / 2 - (len(SUBTITLE) * SUBTITLE_FONT_SIZE) / 2, MARGIN * 2
-                                          + TITLE_FONT_SIZE)
+        self.rendered_subtitle = self.font_subtitle.render(SUBTITLE_PREFIX, 1, BLUE_MONKEY_COLOR)
+        self.monkeypatch = pg.transform.scale(
+            conf.graphics_cache.get_image(Image(conf.media_path("monkeypatch_subtitle.png"))),
+            SUBTITLE_PICTURE_SIZE.as_ints())
+        self.subtitle_pos = Vector.create(conf.pixel_size.x / 2 - SUBTITLE_SIZE.x / 2, MARGIN * 2 + TITLE_FONT_SIZE)
+        self.subtitle_text_pos = self.subtitle_pos + Vector.create(0, SUBTITLE_PICTURE_SIZE.y - SUBTITLE_FONT_SIZE - 5)
+        self.subtitle_pic_pos = self.subtitle_pos + Vector.create((len(SUBTITLE_PREFIX)) * SUBTITLE_FONT_SIZE, 0)
 
     def update(self, sim: Simulator, dt: float, board: Board) -> None:
         self.screen.fill(BLACK_COLOR,
@@ -37,7 +44,8 @@ class TitleBarDisplaySystem(System):
 
     def display_title(self):
         self.screen.blit(self.rendered_title, self.title_pos.as_ints())
-        self.screen.blit(self.rendered_subtitle, self.subtitle_pos.as_ints())
+        self.screen.blit(self.rendered_subtitle, self.subtitle_text_pos.as_ints())
+        self.screen.blit(self.monkeypatch, self.subtitle_pic_pos.as_ints())
 
     def display_fps(self, clock, board: Board):
         fps = clock.get_fps()
